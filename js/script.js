677e0f685fa082222021d3d5a31a2c054a48e28a
parfenyukmisha <24a_pmya@ztu.edu.ua>
2025-04-08 12:58:34 +0300

    покращення форматування картинок на картках, та початок стилізації йих картинок

--- js/script.js
+++ js/script.js
@@ -86,11 +86,11 @@ function getRandomInt(max) {
                 <p>${item.id}</p>
                 <h3>${item.title}</h3>
                 <hr>
-                <img src="img/vitamins/${item.photo}" alt="">
+                <img src="img/vitamins/${item.photo}" alt="Картинка для ${item.title}" onerror="this.onerror = null; this.src="img/vitamins/defaut.png" ;>
                 <p>${item.description}</p>
 
                 <div>
-                <img src="img/vitamins/${item.schema}" alt="">
+                <img src="img/vitamins/${item.schema}" alt="" onerror = null; this.sc="img/vitamins/default_schema.png";>
                 <p>${"💚". repeat(item.rating)+'🤍'.repeat(5-item.rating)}</p>
                 <p>${item.type}</p>
                 </div>
